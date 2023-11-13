67c9f2c6c2498f074245764513544afcf944f597
saidorif <paymart@MacBook-Pro-paymart.local>
2023-11-13 13:17:27 +0500

    init

---
+++ src/test/java/com/example/UzumDemo/UzumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UzumDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
